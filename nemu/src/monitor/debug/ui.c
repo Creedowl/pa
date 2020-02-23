72d987e5ba698cf0374f5604d4e0cf908a5a4871
tracer-ics2017 <tracer@njuics.org>
2020-02-23 23:01:25 +0800

    >  compile 061810432 creedowl Linux 6f1ae95fcee5 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  23:01:25 up 3 days,  5:19,  0 users,  load average: 0.21, 0.10, 0.04 871cec12636d1b188d024fce96b969f09d4a6cfe

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -39,7 +39,9 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 
 static int cmd_si(char *args) {
-  cpu_exec(1);
+  int n = 1;
+  sscanf(args, "%d", &n);
+  cpu_exec(n);
   return 0;
 }
 
