0e7cf8547adcbf3ca75b9d6224a21f250356c1f7
tracer-ics2017 <tracer@njuics.org>
2020-02-27 15:08:31 +0800

    >  compile 061810432 creedowl Linux 6f1ae95fcee5 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:08:31 up 1 day, 17:26,  0 users,  load average: 0.00, 0.03, 0.00 17d520b1c5d40a3d7de9aa6d9d19bca7adb8760

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -83,7 +83,7 @@ static int cmd_x(char *args) {
   }
   int count = 0;
   paddr_t address = 0x0;
-  if (sscanf(_count, "%d", &count) == -1 || sscanf(_address, "%x", &address) == -1) {
+  if (!sscanf(_count, "%d", &count) || !sscanf(_address, "%x", &address)) {
     printf("Usage: x [count] [address]\n");
     return 1;
   }
