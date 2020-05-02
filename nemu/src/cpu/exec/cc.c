88a43f492d3d21ce46dc20fd9666ec79a20f15ea
tracer-ics2017 <tracer@njuics.org>
2020-05-02 12:26:49 +0800

    >  compile 061810432 creedowl Linux 6f1ae95fcee5 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  12:26:49 up  1:43,  0 users,  load average: 0.36, 0.37, 0.21 d662dbd37b23592839496461467c128f367ddf1

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -21,7 +21,10 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_S:
     case CC_L:
     case CC_LE:
-      TODO();
+    case CC_NE: 
+      *dest = cpu.ZF != 0;
+      break;
+
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
