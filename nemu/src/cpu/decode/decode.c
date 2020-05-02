839ae6bd2c20deda0f899570f765277007bc7e76
tracer-ics2017 <tracer@njuics.org>
2020-05-02 20:36:09 +0800

    >  compile 061810432 creedowl Linux 6f1ae95fcee5 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:36:09 up  9:52,  0 users,  load average: 0.10, 0.10, 0.04 7967a243dc79ae755bf19f5125d352aab73ddb68

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -40,7 +40,7 @@ static inline make_DopHelper(SI) {
    */
   // op->simm = (int)instr_fetch(eip, op->width);
   t0 = instr_fetch(eip, op->width);
-  rtl_sext(&t0, &t0, 1);
+  rtl_sext(&t0, &t0, op->width);
   op->simm = t0;
 
   rtl_li(&op->val, op->simm);
