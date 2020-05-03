2978ba4a25aad736309eec69180141ee43f387c1
tracer-ics2017 <tracer@njuics.org>
2020-05-03 16:21:44 +0800

    >  compile 061810432 creedowl Linux 6f1ae95fcee5 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:21:44 up  5:14,  0 users,  load average: 0.04, 0.18, 0.36 cca33bd0c07058c67cec74225a246df6131fd85

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -52,7 +52,7 @@ make_group(gp2,
   /* 0xf6, 0xf7 */
 make_group(gp3,
     IDEXW(test_I, test, 1), EMPTY, EX(not), EMPTY,
-    IDEXW(SI2E, mul, 1), EMPTY, EMPTY, EX(idiv))
+    EX(mul), EMPTY, EMPTY, EX(idiv))
 
   /* 0xfe */
 make_group(gp4,
