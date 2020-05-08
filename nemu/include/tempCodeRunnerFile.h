5b788a7a5b69a6bd2cb1c51f8a646cb315f3f5b0
tracer-ics2017 <tracer@njuics.org>
2020-05-08 13:55:39 +0800

    >  compile 061810432 creedowl Linux 6f1ae95fcee5 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:55:39 up  3:41,  1 user,  load average: 0.34, 0.35, 1.11 5588fdf42b27516ec8d739e55c48d2568c0fe916

---
+++ nemu/include/tempCodeRunnerFile.h
@@ -0,0 +1,2 @@
+
+#define DIFF_TEST
--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -41,7 +41,8 @@ make_EHelper(or) {
 }
 
 make_EHelper(sar) {
-  rtl_sar(&t0, &id_dest->val, &id_src->val);
+  rtl_sext(&t1, &id_dest->val, id_dest->width);
+  rtl_sar(&t0, &t1, &id_src->val);
   operand_write(id_dest, &t0);
   rtl_update_ZFSF(&t0, id_dest->width);
   // unnecessary to update CF and OF in NEMU
