a9e546f1116fad1f4586ee9f1e1021ff6cfc7569
tracer-ics2017 <tracer@njuics.org>
2020-05-08 16:19:41 +0800

    >  run 061810432 creedowl Linux 6f1ae95fcee5 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:19:41 up  6:05,  1 user,  load average: 0.14, 0.37, 1.34 4337e7a78c1ee39f362154af82c50d317b573833

--- nexus-am/tests/cputest/tests/test.c
+++ nexus-am/tests/cputest/tests/test.c
@@ -5,5 +5,7 @@ int main() {
   nemu_assert(a >> 20 == 0xfffff800);
   uint32_t b = 0x80000000;
   nemu_assert(b >> 20 == 0x00000800);
+  int8_t c = 0x80;
+  nemu_assert(c >> 5 == 0xfc);
   return 0;
 }
