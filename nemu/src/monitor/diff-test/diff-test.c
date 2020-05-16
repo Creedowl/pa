713bfd0cf6e44f934d0bf35e4f82ecae12c1f852
tracer-ics2017 <tracer@njuics.org>
2020-05-16 16:37:30 +0800

    >  compile 061810432 creedowl Linux creedowl-manjaro 5.6.10-3-MANJARO #1 SMP PREEMPT Sun May 3 11:03:28 UTC 2020 x86_64 GNU/Linux  16:37:29 up  1:08,  5 users,  load average: 0.22, 0.39, 0.27 bec88a9f244746d6f34ee8c2cbe1549e4ca5d68e

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -48,7 +48,7 @@ void diff_test_skip_nemu() { is_skip_nemu = true; }
     printf("\033[31mdiff test failed, eflags\n"); \
     printf("eip: 0x%08x\n", cpu.eip); \
     printf("nemu.%s: %d\n", #eflag, cpu.eflag); \
-    printf("qemu.%s: %d\033[0m\n", #eflag, qeflags); \
+    printf("qemu.%s: %x\033[0m\n", #eflag, qeflags); \
   }
 
 static uint8_t mbr[] = {
