db489762877be9f41b9e8470a77a7bebd19eb52b
tracer-ics2017 <tracer@njuics.org>
2020-05-16 16:45:14 +0800

    >  compile 061810432 creedowl Linux creedowl-manjaro 5.6.10-3-MANJARO #1 SMP PREEMPT Sun May 3 11:03:28 UTC 2020 x86_64 GNU/Linux  16:45:14 up  1:16,  5 users,  load average: 0.31, 0.21, 0.21 81161d10154f72c56f6ce9c46096f36f3ad7a9e5

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -179,7 +179,7 @@ void difftest_step(uint32_t eip) {
   check_reg(r, eip);
 
   check_eflags(r.eflags, CF, 0);
-  check_eflags(r.eflags, ZF, 5);
+  check_eflags(r.eflags, ZF, 6);
   check_eflags(r.eflags, SF, 7);
   check_eflags(r.eflags, IF, 9);
   check_eflags(r.eflags, OF, 11);
