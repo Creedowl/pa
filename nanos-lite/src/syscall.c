5ace38530af4c215232bb4d96e358dcbe396716f
tracer-ics2017 <tracer@njuics.org>
2020-05-21 20:25:23 +0800

    >  compile 061810432 creedowl Linux creedowl-manjaro 5.6.11-1-MANJARO #1 SMP PREEMPT Wed May 6 10:20:32 UTC 2020 x86_64 GNU/Linux  20:25:23 up  8:20,  9 users,  load average: 0.19, 0.15, 0.22 710f8e4b2c9ff17b8912d4dfe74ffd4ea36c580

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -45,16 +45,17 @@ static inline uintptr_t sys_exit(int code) {
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
 
   switch (a[0]) {
     case SYS_none:
       SYSCALL_ARG1(r) = sys_none();
       break;
     case SYS_exit:
-      sys_exit(a[0]);
+      sys_exit(a[1]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
-  return r;
+  return NULL;
 }
