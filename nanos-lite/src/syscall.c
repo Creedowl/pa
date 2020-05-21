9e0d402351f8f7ed5affb975e6957c5911820680
tracer-ics2017 <tracer@njuics.org>
2020-05-21 21:16:46 +0800

    >  gdb 061810432 creedowl Linux creedowl-manjaro 5.6.11-1-MANJARO #1 SMP PREEMPT Wed May 6 10:20:32 UTC 2020 x86_64 GNU/Linux  21:16:46 up  9:12,  9 users,  load average: 0.08, 0.06, 0.05 317e41ed2406e960f533a799f5e612d453ba951

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,8 +10,14 @@ static inline uintptr_t sys_open(uintptr_t pathname, uintptr_t flags, uintptr_t 
 }
 
 static inline uintptr_t sys_write(uintptr_t fd, uintptr_t buf, uintptr_t len) {
-  TODO();
-  return 1;
+  if(fd == 1 || fd == 2) {
+    int i=0;
+    for(; i<len; i++) {
+      _putc((char)buf + i);
+    }
+    return i;
+  }
+  return -1;
 }
 
 static inline uintptr_t sys_read(uintptr_t fd, uintptr_t buf, uintptr_t len) {
@@ -46,6 +52,8 @@ _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
     case SYS_none:
@@ -53,6 +61,9 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_exit:
       sys_exit(a[1]);
+      break;
+    case SYS_write:
+      SYSCALL_ARG1(r) = sys_write(a[1], a[2], a[3]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
