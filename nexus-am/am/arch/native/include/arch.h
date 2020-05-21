37c2517c85531d18488655124b364412f856f3f0
tracer-ics2017 <tracer@njuics.org>
2020-05-21 16:26:01 +0800

    >  gdb 061810432 creedowl Linux creedowl-manjaro 5.6.11-1-MANJARO #1 SMP PREEMPT Wed May 6 10:20:32 UTC 2020 x86_64 GNU/Linux  16:26:01 up  4:21,  9 users,  load average: 0.15, 0.09, 0.04 8a85e3c098becfc6b0ac0bee78a0e2dacfebd51b

--- nexus-am/am/arch/native/include/arch.h
+++ nexus-am/am/arch/native/include/arch.h
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-typedef unsigned long uintptr_t
+typedef unsigned long uintptr_t;
 
 struct _RegSet {
   uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
