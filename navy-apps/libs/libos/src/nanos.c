e9a9bbbfcaebf2f11ed418335936c3111eb6d4c1
tracer-ics2017 <tracer@njuics.org>
2020-05-22 22:54:58 +0800

    >  gdb 061810432 creedowl Linux creedowl-manjaro 5.6.12-1-MANJARO #1 SMP PREEMPT Sun May 10 14:36:43 UTC 2020 x86_64 GNU/Linux  22:54:58 up 11:36, 10 users,  load average: 0.47, 0.18, 0.12 b1f98632d95283d85dadfa77a2bf1c21a64adc6a

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -40,9 +40,9 @@ void *_sbrk(intptr_t increment) {
   if(res != 0) return (void *)-1;
   _break += increment;
 
-  // char a[40];
-  // sprintf(a, "inc %x res %x old %x end %x\n\0", increment, res, old, _break);
-  // write(1, a, strlen(a));
+  char a[40];
+  sprintf(a, "inc %x res %x old %x end %x\n\0", increment, res, old, _break);
+  write(1, a, strlen(a));
 
   return (void *)old;
 }
