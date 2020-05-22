71c1a133b81941087483ffeb9397d9a26b481998
tracer-ics2017 <tracer@njuics.org>
2020-05-22 20:41:29 +0800

    >  compile 061810432 creedowl Linux creedowl-manjaro 5.6.12-1-MANJARO #1 SMP PREEMPT Sun May 10 14:36:43 UTC 2020 x86_64 GNU/Linux  20:41:29 up  9:22, 10 users,  load average: 0.24, 0.05, 0.02 a089d552b8372812e4573d21e67d015fc2ef253

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -36,7 +36,8 @@ static inline uintptr_t sys_close(uintptr_t fd) {
 }
 
 static inline uintptr_t sys_brk(uintptr_t new_brk) {
-  return mm_brk(new_brk);
+  // return mm_brk(new_brk);
+  return 0;
 }
 
 static inline uintptr_t sys_none() {
