8440a92cc6f99a18c682a851a99155912d7b4bb2
tracer-ics2017 <tracer@njuics.org>
2020-05-22 15:35:35 +0800

    >  gdb 061810432 creedowl Linux creedowl-manjaro 5.6.12-1-MANJARO #1 SMP PREEMPT Sun May 10 14:36:43 UTC 2020 x86_64 GNU/Linux  15:35:34 up  4:16, 10 users,  load average: 0.79, 0.86, 0.61 da49d24a902732e2836383eb3f252f8c928fed93

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -34,13 +34,13 @@ int _write(int fd, void *buf, size_t count) {
 
 void *_sbrk(intptr_t increment) {
   char a[20];
-  memset(a, '\0', 20);
-  sprintf(a, "inc %x\n", increment);
-  write(1, a, 20);
+  // memset(a, '\0', 20);
+  // sprintf(a, "inc %x\n", increment);
+  // write(1, a, 20);
   int res = _syscall_(SYS_brk, end + increment, 0, 0);
-  memset(a, '\0', 20);
-  sprintf(a, "res %d\n", res);
-  write(1, a, 20);
+  // memset(a, '\0', 20);
+  // sprintf(a, "res %d\n", res);
+  // write(1, a, 20);
   if(res != 0) return (void *)-1;
   intptr_t old = (intptr_t)&end;
   end += increment;
