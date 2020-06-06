a0cf798385447ae337b1c56d5678d20529859345
tracer-ics2017 <tracer@njuics.org>
2020-06-06 20:17:40 +0800

    >  compile 061810432 creedowl Linux creedowl-manjaro 5.6.15-1-MANJARO #1 SMP PREEMPT Wed May 27 20:38:56 UTC 2020 x86_64 GNU/Linux  20:17:40 up  6:52,  6 users,  load average: 0.06, 0.04, 0.12 b9ba335f2f20eff23f12b6e91763ffe6771a426

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,16 +1,28 @@
 #include "common.h"
 #include "fs.h"
+#include "memory.h"
 
-#define DEFAULT_ENTRY ((void *)0x4000000)
+// #define DEFAULT_ENTRY ((void *)0x4000000)
+#define DEFAULT_ENTRY ((void *)0x8048000)
 
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
-extern size_t get_ramdisk_size();
+// extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
   int fd = fs_open(filename, 0, 0);
-  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  // fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  int size = fs_filesz(fd);
+  void *pa;
+  void *va = DEFAULT_ENTRY;
+  for(int i=0; i<size; i+= PGSIZE) {
+    pa = new_page();
+    Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
+    _map(as, va, pa);
+    fs_read(fd, pa, i < PGSIZE ? i : PGSIZE);
+    va += PGSIZE;
+  }
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
