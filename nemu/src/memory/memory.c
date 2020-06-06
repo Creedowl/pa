f560e7cd53f3d107596bac2ebf987ea377f0fb4b
tracer-ics2017 <tracer@njuics.org>
2020-06-06 16:15:11 +0800

    >  compile 061810432 creedowl Linux creedowl-manjaro 5.6.15-1-MANJARO #1 SMP PREEMPT Wed May 27 20:38:56 UTC 2020 x86_64 GNU/Linux  16:15:11 up  2:49,  8 users,  load average: 0.10, 0.05, 0.01 ecdf11ad3da06208d694f08c3eba2ebfe005c6e2

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -10,6 +10,35 @@
 
 uint8_t pmem[PMEM_SIZE];
 
+paddr_t page_translate(vaddr_t vaddr, bool is_write) {
+  Log("vaddr: 0x%08x is_write: %d", vaddr, is_write);
+  uint32_t index = vaddr >> 22;
+  uint32_t page = (vaddr >> 12) & 0x3ff;
+  uint32_t offset = vaddr & 0xfff;
+  Log("index: %x page: %x offset: %x", index, page, offset);
+  PDE pde;
+  uint32_t pde_addr = (cpu.cr3.page_directory_base << 12) + index * 4;
+  Log("pde_addr: 0x%08x", pde_addr);
+  pde.val = paddr_read(pde_addr, 4);
+  assert(pde.present);
+  PTE pte;
+  uint32_t pte_addr = (pde.page_frame << 12) + page * 4;
+  Log("pte_addr: 0x%08x", pte_addr);
+  pte.val = paddr_read(pte_addr, 4);
+  assert(pte.present);
+  if(pde.accessed == 0) {
+    pde.accessed = 1;
+    paddr_write(pde_addr, 4, pde.val);
+  }
+  if(pte.accessed == 0 || (pte.dirty == 0 && is_write)) {
+    pte.accessed = 1;
+    pte.dirty = 1;
+    paddr_write(pte_addr, 4, pte.val);
+  }
+  Log("addr 0x%08x", (pte.page_frame << 12) + offset);
+  return (pte.page_frame << 12) + offset;
+}
+
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
@@ -25,9 +54,26 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
+  if(cpu.cr0.paging) {
+    if(false) {
+      assert(0);
+    } else {
+      paddr_t paddr = page_translate(addr, false);
+      return paddr_read(paddr, len);
+    }
+  }
   return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
+  if(cpu.cr0.paging) {
+    if(false) {
+      assert(0);
+    } else {
+      paddr_t paddr = page_translate(addr, true);
+      paddr_write(paddr, len, data);
+      return;
+    }
+  }
   paddr_write(addr, len, data);
 }
