c6d97e596ab4519d610fb7a1d45fffb16ae8910c
061810432-HuangYuxuan <creedowl@gmail.com>
2020-06-06 20:46:06 +0800

    loader

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -5,7 +5,7 @@
 // #define DEFAULT_ENTRY ((void *)0x4000000)
 #define DEFAULT_ENTRY ((void *)0x8048000)
 
-extern void ramdisk_read(void *buf, off_t offset, size_t len);
+// extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
 // extern size_t get_ramdisk_size();
 
