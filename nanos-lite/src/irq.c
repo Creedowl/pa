7843628df3841a2d651f88a8be83983dde04d213
tracer-ics2017 <tracer@njuics.org>
2020-06-19 10:53:23 +0800

    >  gdb 061810432 creedowl Linux creedowl-manjaro 5.6.16-1-MANJARO #1 SMP PREEMPT Wed Jun 3 14:26:28 UTC 2020 x86_64 GNU/Linux  10:53:23 up 13:09,  9 users,  load average: 1.38, 0.84, 0.48 bc4de54a6218bea573ee7d9727de2df5bd6170c6

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -7,7 +7,8 @@ extern _RegSet* do_syscall(_RegSet *r);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:
-      return do_syscall(r);
+      do_syscall(r);
+      return schedule(r);
     case _EVENT_TRAP:
       // Log("event trap");
       return schedule(r);
