5bc4756745cbedc3efdbf693fc85abeb46421b5a
tracer-ics2017 <tracer@njuics.org>
2020-06-19 10:46:03 +0800

    >  gdb 061810432 creedowl Linux creedowl-manjaro 5.6.16-1-MANJARO #1 SMP PREEMPT Wed Jun 3 14:26:28 UTC 2020 x86_64 GNU/Linux  10:46:03 up 13:01,  9 users,  load average: 0.21, 0.16, 0.20 bf8d1f28e44563ad692da3847b577fa54e895a6

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -15,9 +15,9 @@ void load_prog(const char *filename) {
   uintptr_t entry = loader(&pcb[i].as, filename);
 
   // TODO: remove the following three lines after you have implemented _umake()
-  _switch(&pcb[i].as);
-  current = &pcb[i];
-  ((void (*)(void))entry)();
+  // _switch(&pcb[i].as);
+  // current = &pcb[i];
+  // ((void (*)(void))entry)();
 
   _Area stack;
   stack.start = pcb[i].stack;
@@ -29,6 +29,7 @@ void load_prog(const char *filename) {
 _RegSet* schedule(_RegSet *prev) {
   current->tf = prev;
   current = &pcb[0];
+  _switch(&current->as);
   // Log("%x %x", prev, current->tf);
   return current->tf;
 }
